b5e73aa60e0e4ee6705d46fa68ef70a76fdc17dc
Ali Nadjar <ali.nadjar95@gmail.com>
2020-05-28 22:21:57 +0430

    pre-loader added :)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="">
-        <h3 style={{ fontFamily: 'IRANSans' }}>عنوان تبرک </h3>
-      </header>
-    </div>
-  );
+  	//Hide Loading Box (Preloader)
+    function handlePreloader() {
+      if($('.preloader').length){
+        $('.preloader').delay(2000).fadeOut(500);
+      }
+    }
+
+class App extends Component {
+
+
+
+  componentDidMount() {
+    $(window).on('load', function () {
+      handlePreloader();
+      // fixedNavStyle();
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className='preloader'></div>
+        <header className="">
+          <h3 style={{ fontFamily: 'IRANSans' }}>عنوان تبرک </h3>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
